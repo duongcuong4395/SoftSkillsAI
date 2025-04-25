2381aa61aaa99c7c469e0c38bfbdbc8d2bec4fd3
duongcuong4395 <dangduongcuong@gmail.com>
2025-04-25 22:46:38 +0700

    init

---
+++ SoftSkillsAI/SoftSkillsAI/SoftSkillsAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftSkillsAIApp.swift
+//  SoftSkillsAI
+//
+//  Created by Macbook on 25/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SoftSkillsAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
